a551daac976b9a847c424f22814ed007d3e9743c
barbara <barbara@bmoah001@gmail.com>
2021-08-31 15:51:28 +0200

    fixed location context

--- src/screens/home/home.screen.js
+++ src/screens/home/home.screen.js
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
 import { SafeArea } from '../../components/utility/safe-area.components';
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import { InputComponent } from '../../components/form/input.component';
-import { Button, TextInput } from 'react-native-paper';
 
 export const HomeScreen = ({ navigation }) => {
   return (
--- src/screens/home/results.screen.js
+++ src/screens/home/results.screen.js
@@ -1,26 +1,28 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import { SafeArea } from '../../components/utility/safe-area.components';
 import React, { useContext, useState, useEffect } from 'react';
-import { InputComponent } from '../../components/form/input.component';
-import { Button, TextInput, Card } from 'react-native-paper';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
 import { vaccineComputation } from '../../services/vaccine.service';
+import { LocationContext } from '../../context/results.context';
+
 import { getInfo } from '../../services/requests.service';
 
-import { CardContainer, ImageCard, ResultCard } from '../../components/style/result-card.component';
-
-
-
+import { CardContainer, ResultCard } from '../../components/style/result-card.component';
 
 export const ResultsScreen = ({ route }) => {
   // eslint-disable-next-line react/prop-types
-  const { age, sex, location } = route.params;
+
+  const { age, sex, area } = route.params;
+  // const { location, setLocation } = useState();
+  const { location } = useContext(LocationContext);
+
   const result = vaccineComputation(age, sex);
 
   useEffect(() => {
-    const result = vaccineComputation(age, sex, location);
-    getInfo();
-    console.log('text', age, sex, result);
-  }, [age, sex]);
+    const result = vaccineComputation(age, sex, area);
+  }, [location]);
 
   return (
     <SafeArea>
@@ -28,7 +30,7 @@ export const ResultsScreen = ({ route }) => {
         <ResultCard>
           <Text>Age: {age} </Text>
           <Text>Sex: {sex} </Text>
-          <Text>Location: {location} </Text>
+          <Text>Location: {area} </Text>
           <Text>Result: {result} </Text>
         </ResultCard>
       </CardContainer>
