d79578d3df61e413accf4e3c7feb8a25790f7bdb
barbara <barbara@bmoah001@gmail.com>
2021-09-06 15:01:10 +0200

    added/mapped cards plus added percentage

--- src/services/vaccine.service.js
+++ src/services/vaccine.service.js
@@ -1,17 +1,29 @@
 export const vaccineComputation = (age, sex) => {
   if (sex == 'Male') {
     if (age >= 12) {
-      return 'Astrazeneca';
+      return [
+        { vaccine: 'Johnson & Johnson', risk: '0.0003%' },
+        { vaccine: 'Pfizer', risk: '0.01%' },
+        { vaccine: 'Moderna', risk: '0.01%' },
+      ];
     }
   }
 
   if (sex == 'Female') {
     if (age >= 12 && age <= 60) {
-      return 'Pfizer/Biontech';
+      return [
+        { vaccine: 'Johnson & Johnson', risk: '0.3%' },
+        { vaccine: 'Pfizer', risk: '0.0001%' },
+        { vaccine: 'Moderna', risk: '0.00001%' },
+      ];
     }
     if (age < 60) {
       console.log('hit!');
-      return 'Pfizer/Biontech or Astrazeneca';
+      return [
+        { vaccine: 'Johnson & Johnson', risk: '0.00003%' },
+        { vaccine: 'Pfizer', risk: '0.0001%' },
+        { vaccine: 'Moderna', risk: '0.00001%' },
+      ];
     }
   } else {
     console.log('no hit!', age, sex);
