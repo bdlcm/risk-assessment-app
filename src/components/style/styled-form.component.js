5aaaaa33f31da6c2d5bdb5c82d59deec784808d9
barbara <barbara@bmoah001@gmail.com>
2021-09-14 13:56:31 +0200

    next button disabled if not options chosen

--- src/components/style/styled-form.component.js
+++ src/components/style/styled-form.component.js
@@ -2,12 +2,22 @@
 import styled from 'styled-components/native';
 import { Button, TextInput, Switch } from 'react-native-paper';
 import { colors } from '../../infrastructure/theme/colors';
+import SelectDropdown from 'react-native-select-dropdown';
+
+
+export const FormDropdown = styled(SelectDropdown).attrs({
+
+  
+})`
+  text-align: center;
+   `;
 
 export const FormContainer = styled.View`
   background-color: rgba(255, 255, 255, 0.7);
   padding: ${(props) => props.theme.space[4]};
   width: 100%;
   height: 100%;
+  display:flex;
 `;
 
 export const FormVeil = styled.View`
@@ -20,7 +30,8 @@ export const FormVeil = styled.View`
 
 export const FormButton = styled(Button).attrs({})`
   background-color: #C72C62;
-  
+  align-self: center;
+
   padding: ${(props) => props.theme.space[2]};
   margin-bottom: 5px;
   margin-top: 15px;
@@ -28,8 +39,7 @@ export const FormButton = styled(Button).attrs({})`
   text-align: center;
   position: absolute;
   bottom: 10%;
-  left: 10%;
-`;
+ `;
 
 export const AccountButton = styled(Button).attrs({
   color: colors.brand.primary,
@@ -49,4 +59,6 @@ const y = 0;
 
 export const FormCenter = styled.View`
   position: relative;
-`;
+  display: flex;
+
+ `;
