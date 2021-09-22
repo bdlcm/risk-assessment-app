ed8d8d06cd58c43d87421027ffc4406b78324262
barbara <barbara@bmoah001@gmail.com>
2021-09-22 15:08:41 +0200

    fixed order of cards

--- src/services/vaccine.service.js
+++ src/services/vaccine.service.js
@@ -3,15 +3,14 @@ export const vaccineComputation = (age, sex) => {
     if (age > 50) {
       return [
         {
-          vaccine: 'Johnson & Johnson',
-          color: 3,
+          vaccine: 'Moderna',
+          color: 2,
           case: [
-            { risk: 7.8, se: 'Cerebral Thrombosis' },
-            { risk: 1.3, se: 'General Thrombosis' },
-            { risk: 13.6, se: 'Guillain Barre' },
+            { risk: undefined, se: 'Cerebral Thrombosis' },
+            { risk: undefined, se: 'General Thrombosis' },
+            { risk: 2, se: 'Guillain Barre' },
           ],
         },
-
         {
           vaccine: 'Pfizer/Biontech',
           color: 2,
@@ -21,14 +20,13 @@ export const vaccineComputation = (age, sex) => {
             { risk: 9.7, se: 'Guillain Barre' },
           ],
         },
-
         {
-          vaccine: 'Moderna',
-          color: 2,
+          vaccine: 'Johnson & Johnson',
+          color: 3,
           case: [
-            { risk: undefined, se: 'Cerebral Thrombosis' },
-            { risk: undefined, se: 'General Thrombosis' },
-            { risk: 2, se: 'Guillain Barre' },
+            { risk: 7.8, se: 'Cerebral Thrombosis' },
+            { risk: 1.3, se: 'General Thrombosis' },
+            { risk: 13.6, se: 'Guillain Barre' },
           ],
         },
       ];
@@ -36,25 +34,6 @@ export const vaccineComputation = (age, sex) => {
 
     if (age <= 50) {
       return [
-        {
-          vaccine: 'Johnson & Johnson',
-          color: 3,
-          case: [
-            { risk: 7.8, se: 'Cerebral Thrombosis' },
-            { risk: 1.3, se: 'General Thrombosis' },
-            { risk: 9.7, se: 'Guillain Barre' },
-          ],
-        },
-
-        {
-          vaccine: 'Pfizer/Biontech',
-          color: 2,
-          case: [
-            { risk: undefined, se: 'Cerebral Thrombosis' },
-            { risk: undefined, se: 'General Thrombosis' },
-            { risk: 1.3, se: 'Guillain Barre' },
-          ],
-        },
         {
           vaccine: 'Moderna',
           color: 2,
@@ -65,6 +44,24 @@ export const vaccineComputation = (age, sex) => {
             { risk: 1.4, se: 'Guillain Barre' },
           ],
         },
+        {
+          vaccine: 'Pfizer/Biontech',
+          color: 2,
+          case: [
+            { risk: undefined, se: 'Cerebral Thrombosis' },
+            { risk: undefined, se: 'General Thrombosis' },
+            { risk: 1.3, se: 'Guillain Barre' },
+          ],
+        },
+        {
+          vaccine: 'Johnson & Johnson',
+          color: 3,
+          case: [
+            { risk: 7.8, se: 'Cerebral Thrombosis' },
+            { risk: 1.3, se: 'General Thrombosis' },
+            { risk: 9.7, se: 'Guillain Barre' },
+          ],
+        },
       ];
     }
   }
@@ -72,17 +69,6 @@ export const vaccineComputation = (age, sex) => {
   if (sex == 'Female') {
     if (age <= 50) {
       return [
-        {
-          vaccine: 'Johnson & Johnson',
-          color: 3,
-
-          case: [
-            { risk: 16.5, se: 'Cerebral Thrombosis' },
-            { risk: 1.9, se: 'General Thrombosis' },
-            { risk: 6.8, se: 'Guillain Barre' },
-          ],
-        },
-
         {
           vaccine: 'Pfizer/Biontech',
           color: 2,
@@ -103,23 +89,22 @@ export const vaccineComputation = (age, sex) => {
             { risk: 2.1, se: 'Guillain Barre' },
           ],
         },
+        {
+          vaccine: 'Johnson & Johnson',
+          color: 3,
+
+          case: [
+            { risk: 16.5, se: 'Cerebral Thrombosis' },
+            { risk: 1.9, se: 'General Thrombosis' },
+            { risk: 6.8, se: 'Guillain Barre' },
+          ],
+        },
       ];
     }
 
     if (age > 50) {
       console.log('hit!');
       return [
-        {
-          vaccine: 'Johnson & Johnson',
-          color: 3,
-
-          case: [
-            { risk: 14.6, se: 'Cerebral Thrombosis' },
-            { risk: 2, se: 'General Thrombosis' },
-            { risk: 9.3, se: 'Guillain Barre' },
-          ],
-        },
-
         {
           vaccine: 'Pfizer/Biontech',
           color: 2,
@@ -139,6 +124,16 @@ export const vaccineComputation = (age, sex) => {
             { risk: undefined, se: 'Cerebral Thrombosis' },
             { risk: undefined, se: 'General Thrombosis' },
             { risk: 2.5, se: 'Guillain Barre' },
+          ],
+        },
+        {
+          vaccine: 'Johnson & Johnson',
+          color: 3,
+
+          case: [
+            { risk: 14.6, se: 'Cerebral Thrombosis' },
+            { risk: 2, se: 'General Thrombosis' },
+            { risk: 9.3, se: 'Guillain Barre' },
           ],
         },
       ];
