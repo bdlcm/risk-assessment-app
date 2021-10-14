f8f794699302594ddfa0d0a036fa6c07fbedb252
barbara <barbara@bmoah001@gmail.com>
2021-10-14 14:13:48 +0200

    added sources

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -2,15 +2,18 @@ import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../../screens/home/home.screen';
 import { ResultsScreen } from '../../screens/home/results.screen';
+import { SourcesScreen } from '../../screens/home/sources.screen';
+
 import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 export const Navigator = () => (
   <NavigationContainer>
-    <Stack.Navigator >
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={HomeScreen} />
       <Stack.Screen name="Results" component={ResultsScreen} />
+      <Stack.Screen name="Sources" component={SourcesScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
